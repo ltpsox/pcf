8602ec377f1a276cabbb29aae07e7e5b08573599
LTP <LTP@aemcorp.com>
2016-03-01 15:33:29 -0500

    Update uploader_deploy.rb

--- docker/recipes/uploader_deploy.rb
+++ docker/recipes/uploader_deploy.rb
@@ -1,6 +1,6 @@
 docker_image 'amara/uploader' do
   action :pull
-  notifies :redploy, 'docker_container[uploader]', :immediately
+  notifies :redeploy, 'docker_container[uploader]', :immediately
 end
 
 docker_container 'uploader' do
